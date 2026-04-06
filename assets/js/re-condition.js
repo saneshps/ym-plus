02e002861cfcc0e7dced8b58d1122f809e025c8b
saneshps <74853379+saneshps@users.noreply.github.com>
2026-04-06 12:41:13 +0530

    first commit

---
+++ assets/js/re-condition.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".re-conditionSwiper", {
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
